98648eafff9a798d83e8bc784c8710485b9957e2
alicankustemur <alican.kustemur@gmail.com>
2016-04-22 14:24:54 +0300

    first commit

---
+++ src/main/java/com/itirafadresi/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.itirafadresi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController
+{
+
+	@RequestMapping("/")
+	public String view(Model model)
+	{
+		model.addAttribute("title", "İtiraf Adresi");
+		return "index";
+	}
+
+}
